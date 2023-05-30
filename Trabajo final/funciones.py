6d7fab95d9d42d768e9d500f1daff48d9250f793
Lau00Ag <laura.acevedog@udea.edu.co>
2023-05-30 00:37:30 -0500

    Funciones de validación

---
+++ Trabajo final/funciones.py
@@ -0,0 +1,87 @@
+## funciones de validación
+
+
+def validacionNum(x):
+    """Función que valida que un ingreso sea de tipo numérico"""
+    while True:
+        if type(x) == int or type(x) == float:
+            if x >= 0:
+                break
+            else:
+                x = input("Este dato no permite valores negativos. Ingrese de nuevo: ")
+                continue 
+        else:
+            try:
+                x = float(x)
+            except:
+                x = input("El valor ingresado no es numérico. Ingrese de nuevo: ")
+                continue
+    return x
+
+def validacionEnteros(x):
+    """Función que valida que un ingreso sea un número entero"""
+    while True:
+        try:
+            x = int(x)
+            if x >= 0:
+                break
+            else:
+                x = input("Este dato no permite valores negativos. Ingrese de nuevo: ")
+                continue
+            
+        except ValueError:
+            while True:
+                try:
+                    x = int(input("El valor ingresado no es aceptado, se debe ingresar un valor entero.\n Ingrese de nuevo: "))
+                    break
+                except:
+                    continue
+    return x
+
+def rango1_a(x, a):
+    """Función que valida que el numero ingresado se encuentre en el rango correcto"""
+    while True:
+        if x>=1 and x<=a:
+            break
+    
+        else:
+            try:
+                x = int(input("El valor no se encuentra en el rango de opciones. Ingrese de nuevo la opción deseada: "))
+                continue
+            except:
+                x = input("Valor no válido. Ingrese de nuevo: ")
+                continue        
+    return x
+
+def validacionFlotantes(x):
+    """Función que valida que un ingreso sea un número de punto flotante"""
+    while True:
+        try:
+            x = float(x)
+            break 
+        except ValueError:
+            while True:
+                try:
+                    x = float(input("El valor ingresado no es aceptado, se debe ingresar un valor de punto flotante o decimal.\n Ingrese de nuevo : "))
+                    break
+                except:
+                    continue
+    return x
+
+def validacionCadenas(x):
+    """Función que valida que un ingreso sea de tipo cadena de caracteres"""
+    while True:
+        if x.replace(' ', '').isalpha():
+            break
+        else:
+            try:
+                x = input("El valor ingresado no es una cadena de letras. Ingrese de nuevo: ")
+            except:
+                continue
+    return x
+
+     
+
+
+## funciones CRUD
+
--- Trabajo final/main.py
+++ Trabajo final/main.py
@@ -1,7 +1,20 @@
+"""integrantes:
 
+-Laura Acevedo González
+-Diego Alejandro Jaramillo Arroyave
+
+"""
+
+from funciones import validacionNum
+from funciones import validacionEnteros
+from funciones import rango1_a
+from funciones import validacionFlotantes
+from funciones import validacionCadenas
 import pymongo
 
 uri = "mongodb+srv://informatica1:bio123@cluster0.hj2pgzi.mongodb.net/?retryWrites=true&w=majority"
 
 # Crear cliente que se conecte al servidor
 client = pymongo.MongoClient(uri)
+
+
